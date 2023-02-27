7ea68f61bf74a5b6c77d394608e91f05792d009e
Ray Ozzie <ray@ozzie.net>
2023-02-27 15:05:14 -0500

    sets the time from notehub

--- kiosk.sh
+++ kiosk.sh
@@ -3,10 +3,40 @@ set -x
 
 INTERFACE="-interface i2c"
 PORT="-port /dev/i2c-1"
-NOTECARD="$HOME/dev/note-cli/notecard/notecard $INTERFACE $PORT"
+REQ="$HOME/dev/note-cli/notecard/notecard $INTERFACE $PORT"
 PRODUCT="com.blues.ray:kiosk"
 HUB="i.staging.blues.tools"
 
-RSP=`$NOTECARD '{"req":"hub.set","hub":"'$HUB'","product":"'$PRODUCT'","mode":"continuous","sync":true}'`
-ERR=`echo $RSP | jq .err`
-echo "error is "$ERR
+#
+# Set the Notecard operating parameters
+#
+while [ true ]
+do
+  RSP=`$REQ '{"req":"hub.set","host":"'$HUB'","product":"'$PRODUCT'","mode":"continuous","sync":true}'`
+  ERR=`echo $RSP | jq .err`
+  if [[ "$ERR" == null ]]; then break; fi
+  sleep 1
+done
+
+##
+## Set the time and zone from the Notehub under the assumption
+## that this RPi isn't connected to the network and so it
+## has no idea what time it is.  We need the local time zone
+## so that we know when to download new files without
+## causing a visual disruption during normal hours.
+##
+while [ true ]
+do
+  RSP=`$REQ '{"req":"card.time"}'`
+  ERR=`echo $RSP | jq .err`
+  if [[ "$ERR" == null ]];
+  then
+      TIME=`echo $RSP | jq .time`
+      ZONE=`echo $RSP | jq -r .zone | cut -d "," -f 2`
+      if [[ "$ZONE" != "Unknown" ]]; then break; fi
+  fi
+  sleep 1
+done
+sudo date -s '@'$TIME
+sudo timedatectl set-timezone $ZONE
+
